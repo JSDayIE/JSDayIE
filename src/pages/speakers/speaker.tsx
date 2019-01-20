f98c3b9a5dd1335d465c21bc8ae4aecc2de799df
Remo H. Jansen <remo.jansen@wolksoftware.com>
2019-01-20 17:07:13 +0000

    WIP

---
+++ src/pages/speakers/speaker.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+class Speaker extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        // TODO
+      </React.Fragment>
+    );
+  }
+}
+
+export default Speaker;
