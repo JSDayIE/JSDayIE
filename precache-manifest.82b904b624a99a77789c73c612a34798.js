217b837b7f4831d53659414592c87bd3462de220
remojansen <remo.jansen@wolksoftware.com>
2019-02-27 15:47:42 +0000

    deploy

---
+++ precache-manifest.82b904b624a99a77789c73c612a34798.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c9fb10ce4b31ed00d0dc",
+    "url": "/static/js/main.c9fb10ce.chunk.js"
+  },
+  {
+    "revision": "ec7992297a9cf6513f89",
+    "url": "/static/js/1.ec799229.chunk.js"
+  },
+  {
+    "revision": "c9fb10ce4b31ed00d0dc",
+    "url": "/static/css/main.20ff6544.chunk.css"
+  },
+  {
+    "revision": "3564b1ae3c28f5e641a3a1da01fcf68a",
+    "url": "/index.html"
+  }
+];
