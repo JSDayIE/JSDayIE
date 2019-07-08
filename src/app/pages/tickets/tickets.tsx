49321fef967faa47c7feea89121f73bb51af909a
Remo H. Jansen <remo.jansen@wolksoftware.com>
2019-07-08 08:54:14 +0100

    Content update

--- src/app/pages/tickets/tickets.tsx
+++ src/app/pages/tickets/tickets.tsx
@@ -32,9 +32,9 @@ export class Tickets extends React.Component {
                     }
                   </div>
                   <p>* Fees are displayed excluding 23% VAT</p>
-                  <p>
-                    The tickets will be available very soon! Subscribe to our <Link to={PATHS.newsletter}>Newsletter</Link> or follow us on <a target="_blank" href="https://twitter.com/JSDayIE">Twitter</a>, <a target="_blank" href="https://www.facebook.com/Jsdayie-336263463661254">Facebook</a> or <a target="_blank" href="https://www.linkedin.com/company/jsdayie">LinkedIn</a> to be notified when the tickets become available!
-                  </p>
+                  <a className="btn btn-outline-warning btn-lg" href="https://ti.to/wolk-software-limited/jsdayie-2019/">
+                      Get your ticket now!
+                  </a>
                 </React.Fragment>
               );
             }}
