465b96ee33e3d517b9b91dba4ba37c36af48bdd4
Remo Jansen <rjnasen@bordgais.ie>
2019-09-10 14:07:35 +0100

    Content update

---
+++ precache-manifest.9715b090bc79e86beac0430960056640.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "69c280e51afcd33b40fe",
+    "url": "/static/js/main.69c280e5.chunk.js"
+  },
+  {
+    "revision": "e33d884ec59011087ce1",
+    "url": "/static/js/1.e33d884e.chunk.js"
+  },
+  {
+    "revision": "69c280e51afcd33b40fe",
+    "url": "/static/css/main.9f48bf28.chunk.css"
+  },
+  {
+    "revision": "4a517dac60e33e1a21e3e74025f4a14c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6514f66dcf3c6138bb851a9deff585b2.js"
+  "/precache-manifest.9715b090bc79e86beac0430960056640.js"
 );
 
 workbox.clientsClaim();
